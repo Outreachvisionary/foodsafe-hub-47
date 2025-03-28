6c350e0a6e2360320e3f8661be331e7b896608b9
ComplainceFood <reddym.fsc@gmail.com>
2025-03-28 19:06:32 -0400

    Update apiUtils.ts

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,1 +1,8 @@
-
+export const withTimeout = <T>(promise: Promise<T>, timeoutMs = 15000): Promise<T> => {
+  return Promise.race([
+    promise,
+    new Promise<T>((_, reject) => 
+      setTimeout(() => reject(new Error('Request timed out')), timeoutMs)
+    )
+  ]);
+};
